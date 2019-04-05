9b18b446afda2aee64f4fb3464c9112bb57950ca
Michael Leedahl <michael.leedahl@digitalglobe.com>
2019-04-05 17:56:51 -0600

    Updated scripts

--- deploy/scripts/pipeline-deploy.sh
+++ deploy/scripts/pipeline-deploy.sh
@@ -1,16 +1,28 @@
 #!/usr/bin/env bash
 
+# set script defaults
+AWS_ACCOUNT_ID=495137994720
+PROFILE=rndtools_ogc
+REGION=us-east-1
+
 # Get the environment variables for use in the deployment templates
-export REPO_CENTRAL_NAME=$(aws ssm get-parameters --names "/build/repository/central/name" --profile rndtools_ogc --region us-east-1 | awk '/"Value"/ {print $2}' | sed -e 's/^"//' -e 's/",$//')
-export REPO_SNAPSHOT_NAME=$(aws ssm get-parameters --names "/build/repository/snapshot/name" --profile rndtools_ogc --region us-east-1 | awk '/"Value"/ {print $2}' | sed -e 's/^"//' -e 's/",$//')
-export REPO_CENTRAL_URL=$(aws ssm get-parameters --names "/build/repository/central/url" --profile rndtools_ogc --region us-east-1 | awk '/"Value"/ {print $2}' | sed -e 's/^"//' -e 's/",$//')
-export REPO_SNAPSHOT_URL=$(aws ssm get-parameters --names "/build/repository/snapshot/url" --profile rndtools_ogc --region us-east-1 | awk '/"Value"/ {print $2}' | sed -e 's/^"//' -e 's/",$//')
-export REPO_CENTRAL_PLUGIN_NAME=$(aws ssm get-parameters --names "/build/repository/central/plugin/name" --profile rndtools_ogc --region us-east-1 | awk '/"Value"/ {print $2}' | sed -e 's/^"//' -e 's/",$//')
-export REPO_SNAPSHOT_PLUGIN_NAME=$(aws ssm get-parameters --names "/build/repository/snapshot/plugin/name" --profile rndtools_ogc --region us-east-1 | awk '/"Value"/ {print $2}' | sed -e 's/^"//' -e 's/",$//')
-export REPO_CENTRAL_PLUGIN_URL=$(aws ssm get-parameters --names "/build/repository/central/plugin/url" --profile rndtools_ogc --region us-east-1 | awk '/"Value"/ {print $2}' | sed -e 's/^"//' -e 's/",$//')
-export REPO_SNAPSHOT_PLUGIN_URL=$(aws ssm get-parameters --names "/build/repository/snapshot/plugin/url" --profile rndtools_ogc --region us-east-1 | awk '/"Value"/ {print $2}' | sed -e 's/^"//' -e 's/",$//')
-export CLOUDFORMATION_BUCKET=$(aws ssm get-parameters --names "/build/cloudformation/bucket/name" --profile rndtools_ogc --region us-east-1 | awk '/"Value"/ {print $2}' | sed -e 's/^"//' -e 's/",$//')
-export POWERUSER_ROLE_ARN=$(aws ssm get-parameters --names "/build/poweruser/arn" --profile rndtools_ogc --region us-east-1 | awk '/"Value"/ {print $2}' | sed -e 's/^"//' -e 's/",$//')
+export CLOUDFORMATION_BUCKET=$(aws ssm get-parameters --names "/build/cloudformation/bucket/name" --profile $PROFILE --region $REGION | awk '/"Value"/ {print $2}' | sed -e 's/^"//' -e 's/",$//')
+export POWERUSER_ROLE_ARN=$(aws ssm get-parameters --names "/build/poweruser/arn" --profile $PROFILE --region $REGION | awk '/"Value"/ {print $2}' | sed -e 's/^"//' -e 's/",$//')
 
 # Deploy the stacks
-mvn cloudformation:deploy -P Master
+mvn cloudformation:deploy
+
+# Login to AWS Elastic Container Repository Service
+$(aws ecr get-login --no-include-email --region $REGION)
+
+# Build and Push the Docker container to the AWS Container Registry
+GEOPDP_LATEST=$(aws ssm get-parameters --names "/build/docker/geopdp_latest" --profile $PROFILE --region $REGION | awk '/"Value"/ {print $2}' | sed -e 's/^"//' -e 's/",$//')
+GEOPDP_LATEST=0$GEOPDP_LATEST
+GEOPDP_LATEST=$(expr $GEOPDP_LATEST + 1)
+aws ssm put-parameter --name "/build/docker/geopdp_latest" --value $GEOPDP_LATEST --type "String" --overwrite --profile $PROFILE --region $REGION > /dev/null 2>&1
+docker build --tag geopdp:v$GEOPDP_LATEST .
+docker tag geopdp:v$GEOPDP_LATEST $AWS_ACCOUNT_ID.dkr.ecr.us-east-1.amazonaws.com/geopdp:v$GEOPDP_LATEST
+docker push $AWS_ACCOUNT_ID.dkr.ecr.us-east-1.amazonaws.com/geopdp:v$GEOPDP_LATEST
+
+# Record new version
+echo $AWS_ACCOUNT_ID.dkr.ecr.us-east-1.amazonaws.com/geopdp_latest:v$GEOPDP_LATEST > target/deployed.txt
